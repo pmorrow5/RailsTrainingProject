765a86456b70f35446bf3c6aa99f97f33ec53eda
Patrick Morrow <patrick.morrow5@yahoo.com>
2017-03-29 19:51:30 -0500

    Empty Decks message shows if Deck.empty? is false, decks show if true

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,30 +1,37 @@
 class CardsController < ApplicationController
 	before_action :find_card, only: [:show, :edit, :update, :destroy]
-	before_action :find_deck, only: [:create, :edit, :update, :destroy, :index]
+	before_action :find_deck, only: [:create, :edit, :update, :destroy, :index, :show]
 
 	def find_card
-		@card = Card.find(params[:id])
+	  @card = Card.find(params[:id])
 	end
 
 	def find_deck
-		@deck = Deck.find(params[:deck_id])
+	  @deck = Deck.find(params[:deck_id])
 	end
 
 	def index
-		@cards = Card.all
+	  @cards = Card.all
 	end
 
 	def show
-		@cards = Card.where(deck_id: params[:id])
-
+	  @cards = Card.where(deck_id: params[:id])
 	end
 
 	def edit
-
 	end
 
 	def update
-	end
+      respond_to do |format|
+        if @card.update(card_params)
+          format.html { redirect_to @card, notice: 'Card was successfully updated.' }
+          format.json { render :show, status: :ok, location: @card }
+        else
+          format.html { render :edit }
+          format.json { render json: @card.errors, status: :unprocessable_entity }
+        end
+      end
+    end
 
 	def destroy
 	end
